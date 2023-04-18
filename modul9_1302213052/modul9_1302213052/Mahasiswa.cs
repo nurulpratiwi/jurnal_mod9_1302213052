772961992a06983322d89b1d940539f7fab322bb
nurulpratiwi <nurulpratiwi579@gmail.com>
2023-04-18 15:48:38 +0700

    commit first

---
+++ modul9_1302213052/modul9_1302213052/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace modul9_1302213052
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public List<string> course { get; set; }
+        public int year { get; set; }
+
+        public class Mahasiswa(string name, string nim, List<string> course, int year){
+            this.name = name
+
+            }
+
+
+
+    } 
+}}}
